8f230750e80975e1a8408927788aa262457df377
artslob <artslob@yandex.ru>
2020-03-07 18:02:01 +0300

    add download method

--- cyberbox/routes/files.py
+++ cyberbox/routes/files.py
@@ -2,9 +2,10 @@ from typing import List
 from uuid import UUID, uuid4
 
 from databases import Database
-from fastapi import Depends, File, UploadFile
+from fastapi import Depends, File, HTTPException, UploadFile
 from fastapi.routing import APIRouter
 from pydantic.main import BaseModel
+from starlette.status import HTTP_404_NOT_FOUND
 
 from cyberbox.models import files
 from cyberbox.routes.common import User, get_current_user, get_db
@@ -39,3 +40,16 @@ async def upload_file(
     )
     await db.execute(files.insert().values(values))
     return values
+
+
+@router.get("/download/{file_uid}", response_model=FileModel)
+async def download_file(
+    file_uid: UUID, user: User = Depends(get_current_user), db: Database = Depends(get_db),
+):
+    query = files.select().where((files.c.owner == user.username) & (files.c.uid == file_uid))
+    file = await db.fetch_one(query)
+    if not file:
+        detail = f"File with uuid {str(file_uid)!r} not found"
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=detail)
+
+    return FileModel.parse_obj(file)
