8912d58f00b6c5114c6d9b8125d49d281bb99de4
artslob <artslob@yandex.ru>
2020-03-10 19:35:09 +0300

    use html response class in root route

--- cyberbox/routes/root.py
+++ cyberbox/routes/root.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
 from cyberbox.templates import templates
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("root.html", dict(request=request))
