56a4134556b27a9534b48937bed750d8e8cf2341
artslob <artslob@yandex.ru>
2020-03-11 13:32:13 +0300

    add test for profile info with unknown user

--- tests/test_profile_info.py
+++ tests/test_profile_info.py
@@ -13,3 +13,18 @@ async def test_profile_info(logged_user, client: AsyncClient):
     result = response.json()
     assert isinstance(result, dict)
     assert result["username"] == username
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "headers, expected_details",
+    [
+        [None, {"detail": "Not authenticated"}],
+        [{"Authorization": f"Bearer 123"}, {"detail": "Could not validate access token"}],
+    ],
+)
+async def test_profile_info_unknown_user(client: AsyncClient, headers: dict, expected_details):
+    """ Profile is unreachable without valid authorization info. """
+    response = await client.get("auth/profile", headers=headers)
+    assert response.status_code == 401
+    assert response.json() == expected_details
